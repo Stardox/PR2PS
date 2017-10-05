0c6234d58c0aeb0a153d67e43dc41f5db9988694
Stardox <stardoxgit@gmail.com>
2017-10-05 20:14:07 +0200

    Level Editor - Prepared business layer on top of DbContext.

---
+++ PR2PS.DataAccess/LevelsDataAccess/ILevelsDataAccessEngine.cs
@@ -0,0 +1,15 @@
+﻿using PR2PS.Common.DTO;
+using System;
+
+namespace PR2PS.DataAccess.LevelsDataAccess
+{
+    public interface ILevelsDataAccessEngine : IDisposable
+    {
+        /// <summary>
+        /// Validates and saves the level. New version of level is created if level with same title already exists.
+        /// </summary>
+        /// <param name="userId">Id of user who submitted the level.</param>
+        /// <param name="levelData">Level data to be saved.</param>
+        void SaveLevel(Int64 userId, LevelDataDTO levelData);
+    }
+}
---
+++ PR2PS.DataAccess/LevelsDataAccess/LevelsDataAccessEngine.cs
@@ -0,0 +1,29 @@
+﻿using PR2PS.Common.DTO;
+using System;
+
+namespace PR2PS.DataAccess.LevelsDataAccess
+{
+    public class LevelsDataAccessEngine : ILevelsDataAccessEngine
+    {
+        private LevelsContext dbContext;
+
+        public LevelsDataAccessEngine(LevelsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Dispose()
+        {
+            if (this.dbContext != null)
+            {
+                this.dbContext.Dispose();
+                this.dbContext = null;
+            }
+        }
+
+        public void SaveLevel(Int64 userId, LevelDataDTO levelData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
